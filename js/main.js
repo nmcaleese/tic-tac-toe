c802f0dc8f2eb065264a9b1503d30a4cba9e36ad
Nathan <nmcaleese87@gmail.com>
2022-05-31 06:26:23 -0700

    adds basic html and css board build

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 /*----- constants -----*/
+// clicking will produce either an x or an o
+
 
 
 
 /*----- app's state (variables) -----*/
+// the location of the player's move
+
+// showing either an x or an o on click
+
+// displaying a
 
 
 
@@ -14,7 +21,19 @@
 
 /*----- event listeners -----*/
 
+//listen for a click on each cell
+
 
 
 
 /*----- functions -----*/
+
+// switch between x and o
+
+//on event listener click, fire the above code
+
+// disable a cell once clicked
+
+// display a winner, or loser message
+
+//reset the game
