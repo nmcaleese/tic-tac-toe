79bd8b7c3ebf9e7a31e12ca8d75651d0382ff3d0
Nathan <nmcaleese87@gmail.com>
2022-05-31 15:19:00 -0700

    Adds handleClick funciton to cycle through plus or minus 1 and determine eventTarget

--- js/main.js
+++ js/main.js
@@ -1,66 +1,53 @@
+
 /*----- constants -----*/
-// clicking will produce either an x or an o
-const PLAYER_MOVE = ['x', 'O']
+
+const PLAYER_LOGIC = {
+    '1': 'X',
+    '-1': 'O',
+}
+// console.log(PLAYER_LOGIC[1])
+// console.log(PLAYER_LOGIC[-1])
 
 
 
 /*----- app's state (variables) -----*/
-// the location of the player's move
-let currentCell, usedCell, currentPlayer 
 
-
-//currentPlayer is either 0 or 1 and rotates with function
-
-// showing either an x or an o on click
-
+let board, currentPlayer 
 
 
 /*----- cached element references -----*/
 const gameboard = document.getElementById('gameBoard')
 
+
 /*----- event listeners -----*/
 
-//listen for a click on each cell
 gameboard.addEventListener('click', handleClick) 
-
+ 
 
 
 /*----- functions -----*/
 //function starts the game
 function initGame() {
-    let currentPlayer = 0
+    currentPlayer = 1
+    board = [null, null, null, null, null, null, null, null, null]
 }
 
 
 // determine what cell got clicked
 function handleClick(evt) {
-    //console.log(evt.target)
-    //if (cell is free) {update with current player}
-    //else (alert: already been played)
-}
-
-
-// determine currentPlayer 0 or 1
-function determinePlayer() {
-    let currentPlayer = 0
-    if (currentPlayer === 0) {
-        currentPlayer += 1
-    } else { currentPlayer -= 1}
-    return currentPlayer
-} //broken, only returns 1, currentPlayer will not save to next round
-console.log(determinePlayer())
+    board[evt.target.id] = currentPlayer
+    //updating the DOM with PLAYER_LOGIC[currentPlayer]
+    currentPlayer *= -1
+    console.log(evt.target.id)
+    console.log(board)
+    console.log(currentPlayer)
+    }
 
 
 
-// something like, on click if else statement [if empty add (pick x or y)]
 
 
-//on event listener click, fire the above code
-
-
-// disable a cell once clicked
-
-
+initGame()
 
 
 
