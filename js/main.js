11707fdaef5e8e9f86f4fcc53a5e30e0b617febd
Nathan <nmcaleese87@gmail.com>
2022-06-02 15:35:22 -0700

    segregates concerns, updates render function, removes PLAYER logic from handleClick

--- js/main.js
+++ js/main.js
@@ -1,60 +1,61 @@
-
 /*----- constants -----*/
 // set constants: players 1 and 2
 
 const PLAYERS = {
-    '1': 'My Space, Bitch!',
-    '-1': 'Fuck You!',
-}
-
+  "1": "My Space, Bitch!",
+  "-1": "Fuck You!",
+};
 
 /*----- app's state (variables) -----*/
 
 // set variables
-let board, currentPlayer 
-
+let board, currentPlayer;
 
 /*----- cached element references -----*/
 
 //establishes table as the element gameboardEl
-const gameboardEl = document.getElementById('gameBoard')
+const gameboardEl = document.getElementById("gameBoard");
 
 //establish td's as cellsEl
-const cellsEl = document.querySelectorAll('td')
+const cellsEl = document.querySelectorAll("td");
 
 //establish the button element as a resetEl
-const resetEl = document.querySelector('button')
-
+const resetEl = document.querySelector("button");
 
 /*----- event listeners -----*/
 
-gameboardEl.addEventListener('click', handleClick) 
- 
-resetEl.addEventListener('click', initGame)
+gameboardEl.addEventListener("click", handleClick);
 
+resetEl.addEventListener("click", initGame);
 
 /*----- functions -----*/
 //function starts the game
 function initGame() {
-    currentPlayer = 1
-    board = ['', '', '', '', '', '', '', '', '']
-    render() 
-    }  
- 
-function handleClick(evt) {
-    if(board[evt.target.id] === '') {
-        board[evt.target.id] = currentPlayer
-        evt.target.innerText = PLAYERS[currentPlayer]
-        currentPlayer *= -1
-        } else if (board[evt.target.id] === 1 ) {
-            alert('I said, MY SPACE!!!')
-        } else {alert('Try it again, see what happens!')}
-    } 
-
-
-initGame()
-function render(){
-    cellsEl.forEach(function(cell, idx) {
-        cell.innerText = board[idx]
-     })
+  currentPlayer = 1;
+  board = ["", "", "", "", "", "", "", "", ""];
+  render();
 }
+
+function handleClick(evt) {
+  if (board[evt.target.id] === "") {
+    board[evt.target.id] = currentPlayer;
+    currentPlayer *= -1;
+    render()
+  } else if (board[evt.target.id] === 1) {
+    alert("I said, MY SPACE!!!");
+  } else {
+    alert("Try it again, see what happens!");
+  }
+}
+
+function render() {
+  cellsEl.forEach(function (cell, idx) {
+    if(board[idx]){
+        cell.innerText = PLAYERS[board[idx]];
+    } else {
+        cell.innerText = ''
+    }
+  });
+}
+
+initGame();
